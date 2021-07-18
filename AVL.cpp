d1504a527322f97f20f96ae5c22d6b265df3d553
Alyxhuang <2270029597@qq.com>
2021-07-18 12:09:38 +0800

    修复增删bug，简化、统一debug输出方式

--- AVL.cpp
+++ AVL.cpp
@@ -1,4 +1,12 @@
 // AVL.cpp
+#define mmmDEBUG
+#ifdef mmmDEBUG
+#include<stdio.h> 
+#define PRINT_DBG(...) {fprintf(stderr, "%s %s [%d]:", __FILE__, __FUNCTION__, __LINE__);fprintf(stderr, __VA_ARGS__);fprintf(stderr, "\n");} 
+#else
+#define DEBUG_LOG(module) do{}while(0);
+#endif
+
 template<class T>
 struct Node {
     Node(T d, Node<T>* f = nullptr, Node<T>* l = nullptr, Node<T>* r = nullptr, unsigned char h = 1):
@@ -36,6 +44,14 @@ private:
     NodePointer rebuild34(NodePointer a, NodePointer b, NodePointer c,
         NodePointer T0, NodePointer T1, NodePointer T2, NodePointer T3) {
         // 3 + 4 重构 
+        PRINT_DBG("----3 + 4 rebuild:")
+        PRINT_DBG("a=%d, b=%d, c=%d", a->Key, b->Key, c->Key)
+        // #ifndef BDEBUG
+        // #else
+        // std::cout << "----3 + 4 rebuild:" << std::endl;
+        // std::cout << "a=" << a->Key << ",b=" << b->Key << ",c=" << c->Key << std::endl;
+        // // std::cout << "T0=" << a->Key << ",b=" << b->Key << ",c=" << c->Key << std:endl;
+        // #endif
         a->left = T0; if(T0) T0->father = a;
         a->right = T1; if(T1) T1->father = a;
         c->left = T2; if(T2) T2->father = c;
@@ -43,12 +59,23 @@ private:
         b->left = a; a->father = b;
         b->right = c; c->father = b;
         //updateHeight a,b,c
-        // updateHeightAbove(a);
-        a->height = avlMax(getHeight(a->left), getHeight(a->right)) + 1;
+        updateHeightAbove(a);
+        // a->height = avlMax(getHeight(a->left), getHeight(a->right)) + 1;
         updateHeightAbove(c);
+        PRINT_DBG("travel mid point:")
+        // #ifndef BDEBUG
+        // #else
+        // std::cout << "--------travel mid point:" << std::endl;
+        // #endif
+        dfsHelper(b);
         return b;
     }
     NodePointer reBalance(NodePointer v) {
+        PRINT_DBG("reBalance")
+        // #ifndef BDEBUG
+        // #else
+        // std::cout << "----reBalance" << std::endl;
+        // #endif
         NodePointer p = v->father;
         NodePointer g = p->father;
         if(isLeftChild(p)) {
@@ -61,6 +88,7 @@ private:
                 // / \
                 //T0 T1
                 p->father = g->father;
+                if(g->father) isLeftChild(g) ? (g->father->left = p) : (g->father->right = p);
                 return rebuild34(v, p, g, v->left, v->right, p->right, g->right);
             } else {
                 //      g
@@ -71,6 +99,7 @@ private:
                 //     / \
                 //    T1 T2
                 v->father = g->father;
+                if(g->father) isLeftChild(g) ? (g->father->left = v) : (g->father->right = v);
                 return rebuild34(p, v, g, p->left, v->left, v->right, g->right);
             }
         } else {
@@ -83,6 +112,7 @@ private:
                 //         / \
                 //        T2 T3
                 p->father = g->father;
+                if(g->father) isLeftChild(g) ? (g->father->left = p) : (g->father->right = p);
                 return rebuild34(g, p, v, g->left, p->left, v->left, v->right);
             } else {
                 //      g
@@ -93,6 +123,7 @@ private:
                 //     / \
                 //    T1 T2
                 v->father = g->father;
+                if(g->father) isLeftChild(g) ? (g->father->left = v) : (g->father->right = v);
                 return rebuild34(p, v, g, g->left, v->left, v->right, p->right);
             }
         }
@@ -166,63 +197,143 @@ private:
     
 public:
     void dfs() {
-        std::cout << "dfs: " << std::endl;
+        PRINT_DBG("dfs()")
+        // #ifndef BDEBUG
+        // #else
+        // std::cout << "----dfs() " << std::endl;
+        // #endif
         dfsHelper(getHead());
     }
     void dfsHelper(NodePointer x) {
+        
+        // #ifndef BDEBUG
+        // #else
+        // std::cout << "----dfsHelper()" << std::endl;
+        // #endif
         if(!x) return;
+        PRINT_DBG("dfsHelper()")
+
         std::cout << "current node:" << x->Key <<  "\t" << x->Value << std::endl;
         std::cout << "current height: " << (int)x->height << std::endl;
         std::cout << "left:";
         if(x->left) std::cout << x->left->Key;
+        else std::cout << "null";
         std::cout << "\tright:";
         if(x->right) std::cout << x->right->Key;
+        else std::cout << "null";
         std::cout << std::endl;
+
         dfsHelper(x->left);
         dfsHelper(x->right);
     }
     NodePointer insert(T key, U value) {
+        // DEBUG_LOG("insert key:%d")
+        PRINT_DBG("insert()...")
+        PRINT_DBG("key:%d, value:%d", key, value)
+        // #ifndef BDEBUG
+        // #else
+        // std::cout << "----insert()..." << std::endl;
+        // std::cout << key << ":" << value << std::endl;
+        // #endif
         NodePointer g = nullptr;
         NodePointer v = find(key, g);
         if(v != nullptr) {
+            PRINT_DBG("point is existed! Updating ...")
+            // #ifndef BDEBUG
+            // #else
+            // std::cout << "--------point is existed! Updating ..." << std::endl;
+            // #endif
             v->Value = value;
             return v;
         }
         pair<T, U> Ele(key, value);
         NodePointer insertionNode = new Node<pair<T, U>>(Ele);
         if(!head) {
+            PRINT_DBG("head is null")
+            // #ifndef BDEBUG
+            // #else
+            // std::cout << "--------head is null" << std::endl;
+            // #endif
             head = insertionNode;
             return head;
         } else { 
+            PRINT_DBG("head is exist")
+            // #ifndef BDEBUG
+            // #else
+            // std::cout << "--------head is exist" << std::endl;
+            // #endif
             if(g->Key > insertionNode->Key) {
+                PRINT_DBG("insert to left:%d", g->Key)
+                // #ifndef BDEBUG
+                // #else
+                // std::cout << "------------insert to left:" << g->Key << std::endl;
+                // #endif
                 g->left = insertionNode;
                 insertionNode->father = g;
             } else {
+                PRINT_DBG("insert to right:%d", g->Key)
+                // #ifndef BDEBUG
+                // #else
+                // std::cout << "------------insert to right:" << g->Key << std::endl;
+                // #endif
                 g->right = insertionNode;
                 insertionNode->father = g;
             }
         }
+        PRINT_DBG("updateHeightAbove")
+        // #ifndef BDEBUG
+        // #else
+        // std::cout << "--------updateHeightAbove" << std::endl;
+        // #endif
         updateHeightAbove(insertionNode);
+        dfs();
+        // for(g = insertionNode->father; g; g = g->father) {
+        //     if(!isBalanced(g)) {
+        //         g = reBalance(tallerChild(tallerChild(g)));
+        //     }
+        //     updateHeightAbove(g);
+        // } 
+        int t = 0;
         for(g = insertionNode->father; g; g = g->father) {
+            ++t;
             if(!isBalanced(g)) {
                 g = reBalance(tallerChild(tallerChild(g)));
+            } else if(t > 2) {
+                updateHeightAbove(g);
+                break;
             }
-        } 
+            updateHeightAbove(g);
+        }   
         return insertionNode;
     }
     bool remove(T key) {
         NodePointer g = nullptr;
         NodePointer v = find(key);
-        std::cout << "remove:" << key << std::endl;
+        PRINT_DBG("remove:%d", key)
+        // #ifndef BDEBUG
+        // #else
+        // std::cout << "remove:" << key << std::endl;
+        // #endif
         if(v == nullptr) {
             return false;
         }
+        //前面find已经更新过head
+        if(!head->left && !head->right) {
+            head = nullptr;
+            return true;
+        }
         removeNode(v, g);
         updateHeightAbove(g);
+        int t = 0;
         for(; g; g = g->father) {
+            ++t;
             if(!isBalanced(g)) {
                 g = reBalance(tallerChild(tallerChild(g)));
+            } else if(t > 2) {
+                updateHeightAbove(g);
+                break;
             }
+            updateHeightAbove(g);
         }   
         return true;
     }
@@ -254,15 +365,28 @@ public:
     }
     NodePointer removeNode(NodePointer x, NodePointer& hot) {
         NodePointer rhs = x;
+        //寻找可交换的边缘节点
         if(x->left) {
             rhs = maxNode(x->left);
         } else if(x->right) {
             rhs = minNode(x->right);
         }
+        //交换字段-->交换位置
         if(rhs != x){
             avlSwap(x->Key, rhs->Key);
             avlSwap(x->Value, rhs->Value);
         }
+        //在另外一侧，至多有一个节点，交换位置
+        if(rhs->left) {
+            avlSwap(rhs->left->Key, rhs->Key);
+            avlSwap(rhs->left->Value, rhs->Value);
+            rhs = rhs->left;
+        } else if (rhs->right) {
+            avlSwap(rhs->right->Key, rhs->Key);
+            avlSwap(rhs->right->Value, rhs->Value);
+            rhs = rhs->right;
+        }
+        //现在，rhs已经没有后代节点了
         hot = rhs->father;
         if(isLeftChild(rhs)) {
             hot->left = nullptr;
--- test.cpp
+++ test.cpp
@@ -3,12 +3,25 @@
 //#define NOT_PAIR_ALIAS
 #include "AVL.cpp"
 // #undef NOT_PAIR_ALIAS
+// #define DEBUG
+
+// #define PRINT_DBG(...) {fprintf(stderr, "%s %s [%d]:", __FILE__, __FUNCTION__, __LINE__);fprintf(stderr, __VA_ARGS__);fprintf(stderr, "\n");}
+// #define PRINTF(s, ...) printf(s, ##__VA_ARGS__) 
+// int main() 
+// { 
+// int i =100; 
+// PRINT_DBG("hello")
+// PRINT_DBG("world, %d\t,%s",i,"1w12w1"); 
+// // MACRO("hello, world\n"); //这里没有可变参数， 所以要用##__VA_ARGS__ 
+// // MACRO("hello, %s\n", "fanrey"); 
+// return 0; 
+// } 
 int main() {
-    
-    Node<int> a(10);
-    pair<int, double> b(10, 20);
-    
-    std:: cout << b.fir;
+    // PRINT_DBG(1,"hello\n"); 
+    //Node<int> a(10);
+    //pair<int, double> b(10, 20);
+    //string s;
+    // DEBUG_LOG(123);
 
     map<int, int> m;
     m.insert(1, 1);
@@ -17,7 +30,16 @@ int main() {
     m.insert(4, 4);
     m.insert(5, 5);
     m.insert(6, 6);
-    //m.insert(1, -1);
-    //m.remove(3);
+    m.insert(1, -1);
+    m.remove(3);
+    m.dfs();
+    m.remove(4);
+    m.remove(1);
+    // std::cout << "------------------" << std::endl;
+    m.dfs();
+    // std::cout << "------------------" << std::endl;
+    m.remove(2);
+    m.remove(5);
+    m.remove(6);
     m.dfs();
 }
